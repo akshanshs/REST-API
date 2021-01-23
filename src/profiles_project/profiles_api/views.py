9b35f1bfa94a410fbe56978f9cf5ee9cf99cad60
Akshansh Singh <akshanshsingh@Akshanshs-MBP.fritz.box>
2021-01-23 18:41:57 +0100

    First API view created

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """ test """
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'lol'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+        
